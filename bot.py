5212074fb7cb24c0492b0671fca48a879529932d
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-03-09 13:21:54 +0900

    返信機能を使うようにしてみた

--- bot.py
+++ bot.py
@@ -1,20 +1,17 @@
 from openai import OpenAI
 openai_client = OpenAI()
 
-MODEL = "gpt-4-turbo-preview"
-MESSAGES = []
+MODEL = "gpt-3.5-turbo"
+# MODEL = "gpt-4-turbo-preview"
 
-def chat(user_message: str) -> str:
-    if not MESSAGES:
-        MESSAGES.append({"role": "system", "content": "**重要: 回答は端的に**（長文を求められた場合を除く）"})
-    MESSAGES.append({"role": "user", "content": user_message})
+def chat(message_history: list[dict]) -> str:
+    message_history.insert(0, {"role": "system", "content": "**重要: 回答は端的に**（長文を求められた場合を除く）"})
     response = openai_client.chat.completions.create(
         model=MODEL,
-        messages=MESSAGES,
+        messages=message_history,
         # temperature=0,
     )
     response_message = response.choices[0].message
-    MESSAGES.append(response_message)
     return response_message.content
 
 
@@ -30,28 +27,33 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-# @client.event
-# async def on_ready():
-#     print(f'Logged in as {client.user}')
-
-
-def on_mention(user_content: str) -> str:
-    match user_content:
-        case "reset":
-            MESSAGES.clear()
-            return "resetしました"
-        case _:
-            return chat(user_content)
-
-
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(original_message):
+    if original_message.author == client.user:
         return
-    if client.user in message.mentions:
-        user_content = message.content.replace(f'<@{client.user.id}>', '').strip()
-        reply_content = on_mention(user_content)
-        await message.channel.send(f'{message.author.mention} {reply_content}')
+    #
+    message = original_message
+    message_history = []
+    is_last_message_reply_to_bot = False
+    while True:
+        content = message.content
+        is_last_message_reply_to_bot = client.user in message.mentions
+        content = content.replace(f'<@{client.user.id}>', '').strip()
+        if message.author == client.user:
+            message_history.append({"role": "assistant", "content": content})
+        else:
+            message_history.append({"role": "user", "content": content})
+        if message.reference is None:
+            break
+        message = await message.channel.fetch_message(message.reference.message_id)
+    if not is_last_message_reply_to_bot:
+        return
+    if len(message_history) >= 10:
+        await original_message.reply("（会話長すぎるので打ち切ります...）", mention_author=True)
+        return
+    message_history.reverse()
+    response = chat(message_history)
+    await original_message.reply(response, mention_author=True)
 
 
 client.run(DISCORD_TOKEN)
